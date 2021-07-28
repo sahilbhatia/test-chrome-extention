16be69ed395ca2087e59ef02d5bb72884bfb5c05
Sahil Bhatia <sahil@joshsoftware.com>
2021-07-28 16:22:02 +0530

    e.g. of communication between different contexts

--- popup.js
+++ popup.js
@@ -16,3 +16,44 @@ logoutBtn.addEventListener("click", async () => {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: () => {
+      console.log("Running in context of actual page!");
+
+      const emailSelector = document.querySelectorAll("input[name='email']")?.[0];
+
+      chrome.runtime.sendMessage(
+        { email: emailSelector?.value },
+        function(response) {
+          console.log(response.msg);
+        }
+      );
+    }
+  });
+
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+
+      const email = request.email;
+
+      if (email) {
+        sendResponse({ msg: `Email found to be: ${email}` });
+      } else {
+        sendResponse({ msg: "Email not found!" });
+      }
+
+      const pTag = document.createElement("p");
+      pTag.innerHTML = `<b>Email:</b> ${email}`;
+
+      document.body.prepend(pTag);
+    }
+  );
+});
